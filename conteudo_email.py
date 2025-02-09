e52635e2d53f973e9e2e498df4ddc9c7d73bc1b0
Luiz Gabriel Lisboa Santos <lzg.lisboasantos5569@gmail.com>
2025-02-09 15:30:49 -0300

    Mudando a imagem para uma url pública

--- conteudo_email.py
+++ conteudo_email.py
@@ -1,8 +1,9 @@
 import base64 # para embedar a imagem no html
 
 # Convertendo a imagem para base64, assim podendo enviála embedada no corpo do email
-with open("./assets/odonto_azul.png", "rb") as arquivo_imagem:
-    img_base64 = base64.b64encode(arquivo_imagem.read()).decode("utf-8")
+# Isso só funciona em alguns provedores de email, no outlook por exemplo, já no gmail a imagem precisaria estar hospedada em algum lugar
+# with open("./assets/odonto_azul.png", "rb") as arquivo_imagem:
+#     img_base64 = base64.b64encode(arquivo_imagem.read()).decode("utf-8")
 
 
 def conteudo_html(destinatario_nome):
@@ -11,7 +12,7 @@ def conteudo_html(destinatario_nome):
             <body>
                 <h1>Olá!</h1>
                 <p>Este é um email de teste em HTML enviado para <b>{destinatario_nome}</b> através de uma automatização feita em Python!</p>
-                <img src="data:image/jpeg;base64,{img_base64}" alt="Logo da empresa" width=20% height=20%>
+                <img src="https://app.odontogroup.com.br/assets/media/logos/odonto_azul.png" alt="Logo da empresa" width=20% height=20%>
             </body>
         </html>
     """
