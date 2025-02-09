5835d0e701a289cefc17f2f1d90f9da33b7ab080
Luiz Gabriel Lisboa Santos <lzg.lisboasantos5569@gmail.com>
2025-02-09 14:31:52 -0300

    Transformando o arquivo em um módulo

--- envia_email.py
+++ envia_email.py
@@ -1,37 +1,40 @@
 import os # importando funções do sistema operacional
 from dotenv import load_dotenv # importando funções para o funcionamento da .env
 from email.message import EmailMessage # biblioteca nativa do python
-import ssl # adiciona uma camada de segurança
+import ssl # protocolo de segurança
 import smtplib # biblioteca para o envio de emails
 import json # biblioteca para carregar os dados recebidos por json
 load_dotenv()
 
-# Carregando dados do json
-dados_json = "./dados_email.json" # caminho para o arquivo json
-with open(dados_json, 'r', encoding='utf-8') as f: # traz os dados do json para a variável "dados"
-    dados = json.load(f)
+def envia_email(dados_json):
 
-# Remetente e destinatário
-remetente_email = os.getenv("EMAIL")
-senha_email = os.getenv("SENHA_EMAIL")
-destinatario_email = dados["email"]
-destinatario_nome = dados["nome"]
+    # Carregando dados do json
+    with open(dados_json, 'r', encoding='utf-8') as f: # traz os dados do json para a variável "dados"
+        dados = json.load(f)
 
-# Conteúdo do email
-assunto_email = "Teste"
-body = f"""
-    Este é um email de teste enviado para {destinatario_nome} através de uma automatização feita em Python!
-"""
+    # Remetente e destinatário
+    remetente_email = os.getenv("EMAIL") # pegando meu email e senha da variável de ambiente
+    senha_email = os.getenv("SENHA_EMAIL")
+    destinatario_email = dados["email"]
+    destinatario_nome = dados["nome"]
 
-# Criando um objeto EmailMessage e passando o conteúdo do email
-email = EmailMessage()
-email["From"] = remetente_email
-email["To"] = destinatario_email
-email["Subject"] = assunto_email
-email.set_content(body) # estudar melhor sobre esse objeto
+    # Conteúdo do email
+    assunto_email = "Teste"
+    body = f"""
+        Este é um email de teste enviado para {destinatario_nome} através de uma automatização feita em Python!
+    """
 
-context = ssl.create_default_context() # preciso pesquisar oq isso aq faz exatamente
+    # Criando um objeto EmailMessage e passando o conteúdo do email
+    email = EmailMessage() # cria uma mensagem estruturada como um email
+    email["From"] = remetente_email
+    email["To"] = destinatario_email
+    email["Subject"] = assunto_email
+    email.set_content(body)
 
-with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp: # estudar melhor sobre essa lib
-    smtp.login(remetente_email, senha_email)
-    smtp.sendmail(remetente_email, destinatario_email, email.as_string())
+    context = ssl.create_default_context() # cria um contexto SSL seguro (criptografia).
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp:
+        smtp.login(remetente_email, senha_email)
+        smtp.sendmail(remetente_email, destinatario_email, email.as_string())
+
+    print("Email enviado com sucesso!") # remover isso depois
---
+++ teste_chama_função.py
@@ -0,0 +1,5 @@
+from envia_email import envia_email
+
+dados_json = "./dados_email.json" # caminho para o arquivo json
+
+envia_email(dados_json)
